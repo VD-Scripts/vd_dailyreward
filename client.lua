1dbaf2f9789358d7f82e02e58b5f317b0e96bade
VD-Scripts <117598520+VD-Scripts@users.noreply.github.com>
2024-05-19 16:19:34 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,6 @@
+local rewardData = 0
+local typeshit = ""
+
 RegisterNetEvent("vd:startPlayerReward", function()
     SendNUIMessage({type="openUi"})
     SetNuiFocus(true, true)
@@ -12,6 +15,20 @@ RegisterNUICallback("exit", function()
     SetNuiFocus(false, false)
 end)
 
-RegisterNetEvent("vd:setPlayerHud", function(type)
-    SendNUIMessage({type = type})
+RegisterNetEvent("vd:setPlayerHud", function(type, hours)
+    SendNUIMessage({type = type, hours=hours})
+    typeshit = type
+    if hours then
+        rewardData = hours
+    end
 end)
+
+local function hours()
+    if typeshit == "Invalid" then
+        rewardData = rewardData - 1
+        TriggerServerEvent("vd:updateTime", rewardData)
+    end
+
+    Citizen.SetTimeout(3600 * 1000, hours)
+end
+hours()
